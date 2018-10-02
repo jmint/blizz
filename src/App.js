461d0e1c1fa526316d6699fe0c8d5587cd285429
jmint <joelmiranda1300@gmail.com>
2018-10-02 13:27:24 -0500

    first deployment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react'
 import styles from './App.css'
-import  Strategy from 'passport-bnet'
 var BNET_ID = "fvg59n66a3ndw8scprhxatrdnsx6qzr8"
 var BNET_SECRET = "MDdwRkG6rVSwdAX783yJ5SS26pUWNH3C"
-import passport from 'passport-oauth2'
+var BnetStrategy = require('passport-bnet').Strategy;
+import passport from 'passport'
+import axios from 'axios'
 
 
 class App extends Component {
 
   componentDidMount() {
-    passport.use(new Strategy({
+    passport.use(new BnetStrategy({
       clientID: BNET_ID,
       clientSecret: BNET_SECRET,
-      callbackURL: "https://unruffled-fermi-87364b.netlify.com/",
+      callbackURL: "http://localhost:8080/",
       region: "us"
     }, function (accessToken, refreshToken, profile, done) {
-      return done(null, profile);
+        return done(null, profile);
     }));
+    
+    axios.get('https://us.battle.net/oauth/authorize',
+    passport.authenticate('bnet'))
+    .then(function (response){
+      console.log(response);
+    });
+
+    axios.get('http://localhost:8080/',
+    passport.authenticate('bnet', { failureRedirect: 'http://localhost:8080/' }),
+    function(req, res){
+        res.redirect('http://localhost:8080/');
+        console.log(req, res);
+    });
+
+
+    
   }
 
   render() {
